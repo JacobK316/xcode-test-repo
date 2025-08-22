6a9a95508ef45bbf4206505a6ef225ebc419ec86
Jacob Kappler <jacobkappler@gmail.com>
2025-08-22 11:16:19 -0400

    Initial Commit

---
+++ Hello/HelloApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloApp.swift
+//  Hello
+//
+//  Created by Jacob Kappler on 8/22/25.
+//
+
+import SwiftUI
+
+@main
+struct HelloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
